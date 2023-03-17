34e092912f02d4d2440ec2e1a801c05dbb61eb4e
ClaudyaSalim <claudya.salim@gmail.com>
2023-03-17 12:52:58 +0700

    upload

---
+++ src/pertemuan12/Class1.java
@@ -0,0 +1,28 @@
+package pertemuan12;
+
+public class Class1 extends Thread {
+
+	public Class1() {
+		setDaemon(true); // bikin dari user thread jadi daemon thread
+	}
+	
+	public void run() {
+		System.out.println("Countdown from 10 to 5");
+		
+		int count = 10;
+		
+		while(count>5) {
+			try {
+				sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(count);
+			count--;
+		}
+		
+		boolean cek = isDaemon();
+		System.out.println(cek);
+	}
+
+}
---
+++ src/pertemuan12/Main.java
@@ -0,0 +1,26 @@
+package pertemuan12;
+
+public class Main {
+
+	public Main() {
+		Class2 count = new Class2();
+		Thread countdown2 = new Thread(count);  // bikin thread dari runnable
+		countdown2.start();
+		
+		Class1 countdown1 = new Class1();
+		countdown1.start();
+		
+		countdown2.setPriority(8);
+		countdown1.setPriority(2);
+		
+		System.out.println("Priority of: " + countdown2.getName() + " " + countdown2.getPriority());
+		System.out.println("Priority of: " + countdown1.getName() + " " + countdown1.getPriority());
+	}
+
+	public static void main(String[] args) {
+		
+		new Main();
+
+	}
+
+}
